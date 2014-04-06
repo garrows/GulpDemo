ba9e470823dfdd50162447f09681c6f2a2b68687
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2014-04-07 08:19:47 +1000

    3 (bower) initial commit

---
+++ 3/app/js/index.js
@@ -0,0 +1,11 @@
+
+var thecatapi = require('thecatapi');
+
+var cat = new thecatapi({target:'catDiv'});
+
+
+window.addEventListener('load', function() {
+
+	document.getElementById("outDiv").innerText = "Test 2";
+
+}, false);
---
+++ 3/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp'),
+    browserify = require('gulp-browserify');
+
+var livereloadServer; 
+var livereload = function (_file) {
+    return function (_path) {
+        if (livereloadServer) livereloadServer.changed(_file);
+    }
+}
+
+gulp.task('scripts', function() {
+
+    gulp.src(['./app/js/index.js'])
+        .pipe(browserify({
+            debug : true,
+            "fullPaths": true
+        }))
+        .pipe(gulp.dest('./build/'))
+        .on('end', livereload('.js'));
+
+});
+
+gulp.task('watch', function() {
+
+    livereloadServer = require('gulp-livereload')();
+
+    gulp.watch(['./app/**/*.js'], ['scripts']);
+});
+
+gulp.task('default', [
+    'scripts',
+    'watch'
+]);
+
