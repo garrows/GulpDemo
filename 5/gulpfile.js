2e05247bd618b73c48cd7ac1854bc83c205bf198
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2014-04-07 15:06:34 +1000

    5 (Series & Parallel demo) done

---
+++ 5/gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require('gulp'),
+    tester = require('./gulp-tester');
+
+gulp.task('one', function() {
+    gulp.src(['./testInput.js'])
+        .pipe(tester({ name : "1.1", timeout: 3000 }))
+
+    gulp.src(['./testInput.js'])
+        .pipe(tester({ name : "1.2", timeout: 10 }))
+});
+
+gulp.task('two', function() {
+    gulp.src(['./testInput.js'])
+        .pipe(tester({ name : "2.1", timeout: 1000 }));
+});
+
+gulp.task('three', function() {
+    gulp.src(['./testInput.js'])
+        .pipe(tester({ name : "3.1", timeout: 10 }));
+});
+
+gulp.task('four', ['three'], function() {
+    gulp.src(['./testInput.js'])
+        .pipe(tester({ name : "4.1", timeout: 1000 }));
+});
+
+gulp.task('default', [
+    // 'one',
+    // 'two',
+    'four'
+]);
+
